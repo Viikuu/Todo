301a800404d973937863b32b3ea1bd057a0ae457
Viikuu <viiktorw77@gmail.com>
2023-01-08 12:39:19 +0100

    feat: Add needed middlewares and import providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NoteModule } from './note/note.module';
-import {MongooseModule} from "@nestjs/mongoose";
-import {ConfigModule} from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule } from "@nestjs/config";
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { JwtStrategy } from "./auth/jwt/jwt.strategy";
+
 
 @Module({
   imports: [
       NoteModule,
       ConfigModule.forRoot(),
       MongooseModule.forRoot(process.env.DATABASE_HOST, { dbName: 'todolist' }),
+      UserModule,
+      AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+  ],
+  providers: [
+    JwtStrategy,
+    AppService
+  ],
 })
 export class AppModule {}
